91b1d45bc7179ec7efec8836b5156cadbd5c7964
PDeimel <philipp.deimel@fh-bielefeld.de>
2023-05-20 20:44:38 +0200

    Gave Cake visuals

--- game/src/ecs/items/Cake.java
+++ game/src/ecs/items/Cake.java
@@ -1,8 +1,20 @@
 package ecs.items;
 
+import graphic.Animation;
+
+import java.util.ArrayList;
+
 public class Cake extends ItemData{
 
+    private ArrayList<String> inventoryTexture = new ArrayList<>();
+    private ArrayList<String> worldTexture = new ArrayList<>();
     public Cake() {
         super();
+        super.setItemName("Cake");
+        super.setDescription("A yummy piece of cake that will restore 20 HP.");
+        inventoryTexture.add("objects/treasurechest/items/cake/cake_inventory.png");
+        super.setInventoryTexture(new Animation(inventoryTexture, 5));
+        worldTexture.add("objects/treasurechest/items/cake/cake_world.png");
+        super.setWorldTexture(new Animation(worldTexture, 5));
     }
 }
